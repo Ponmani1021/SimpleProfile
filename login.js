e1081a3dd4b76a254993d3b66fe61d49c0a8c7dd
Ponmani1021 <ponmani1021@gmail.com>
2025-06-14 17:07:38 +0530

    added code

--- login.js
+++ login.js
@@ -2,37 +2,41 @@ let form = document.getElementById("form")
 
 form.addEventListener('submit',function(event){
     event.preventDefault();
-    
-    let userName = document.getElementById("name").value;
-    let wNum = document.getElementById("num").value;
 
+    // getting the values from input box
+    let userName = document.getElementById("name");
+    let wNum = document.getElementById("num");
+
+    //definig default value
     let crtUserName = "Ponmani"
     let crtWNumber = "0123456789"
 
-    if(userName === crtUserName && wNum === crtWNumber){
+    if((userName.value) === crtUserName && (wNum.value) === crtWNumber){
         window.location.href = "janpic.html"   //Navigate to another file
     }
     else{
-        alert("Invalid Username or Whatspp Number")
-        userName.style.borderColor = "red";
-        wNum.style.borderColor = "red";
+        alert("Invalid Username or Whatspp Number");
+        userName.value= "";
+        wNum.value = "";
     }
 })
 
 // for selecting profile image
 
 let submitbutton = document.getElementById("submitbtn")
-let Image = document.getElementById("img")
+let profileImage = document.getElementById("fileupload")
+let previewimg = document.getElementById("previewImage")
 
-submitbutton.addEventListener('click', () => {
-    const file = Image.files[0];
+profileImage.addEventListener('change', () => {
+    const file = profileImage.files[0];
     if(file){
+        
         const reader = new FileReader();
-        reader.onload = function(e){
-            localStorage.setItem('selectedImg',e.target.result)
-            window.location.href = 'janpic.html'
+        reader.onload = function () {
+            previewimg.src = reader.result; 
+            localStorage.setItem('profileImage', reader.result);
         }
-        reader.readAsDataURL(file)
+        reader.readAsDataURL(file);
     }
     else{
         alert('Please select an image first')
@@ -40,7 +44,3 @@ submitbutton.addEventListener('click', () => {
     }
 })
 
-let Gobackbtn = document.getElementById("gobackbtn")
-function Goback(){
-    window.location.href = "login.html"
-}
